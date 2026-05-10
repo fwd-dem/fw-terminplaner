8bd6782dc954964eab0691e7fa72a85384cec197
KarstenBier <hallos-pfannen.62@icloud.com>
2026-05-10 22:20:31 +0200

    Update einladung.js

--- einladung.js
+++ einladung.js
@@ -390,7 +390,7 @@ function buildPDF(doc, FONT, LOGO_FW, LOGO_FFW, measureOnly) {
 
   // ── HEADER ───────────────────────────────────────────────────
   const HEADER_H = 36;
-  const LOGO_H   = 36;
+  const LOGO_H   = 30;
   const LOGO_Y   = (HEADER_H - LOGO_H) / 2;
 
   if (!measureOnly) {
@@ -402,12 +402,8 @@ function buildPDF(doc, FONT, LOGO_FW, LOGO_FFW, measureOnly) {
     }
 
     if (LOGO_FFW) {
-      // Schild ist Hochformat ca. 0.85 breit : 1 hoch
-      const L2_H = LOGO_H;
-      const L2_W = LOGO_H * 0.99;
-      const L2_X = W - margin - L2_W;
-      const L2_Y = (HEADER_H - L2_H) / 2;
-      doc.addImage(LOGO_FFW, "PNG", L2_X, L2_Y, L2_W, L2_H);
+      const L2_X = W - margin - LOGO_H;
+      doc.addImage(LOGO_FFW, "PNG", L2_X, LOGO_Y, LOGO_H, LOGO_H);
     }
 
     const textAreaLeft  = margin + (LOGO_FW ? LOGO_H + 3 : 0);
